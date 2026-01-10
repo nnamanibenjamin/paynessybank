423796f659a2018ca9f8b668fda092c514d0d30a
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2026-01-11 00:53:09 +0100

    first commit

---
+++ src/main/java/com/paynesy/paynesybank/PaynesybankApplication.java
@@ -0,0 +1,13 @@
+package com.paynesy.paynesybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaynesybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaynesybankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paynesy/paynesybank/PaynesybankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paynesy.paynesybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaynesybankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
